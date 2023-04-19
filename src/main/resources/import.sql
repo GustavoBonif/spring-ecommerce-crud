0168c71a6998c89b5768875b740f7bfc566f50f6
GustavoBonif <gubonic@gmail.com>
2023-04-19 09:34:22 -0300

    first init

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO tb_product(name, description, price) VALUES ('TV', 'Uma televisão de 32 polegadas', 1299.99);
+INSERT INTO tb_product(name, description, price) VALUES ('Teclado', 'Teclado de escritório sem fio', 149.99);
+INSERT INTO tb_product(name, description, price) VALUES ('Mouse', 'Mouse sem fio a pilha', 99.99);
+INSERT INTO tb_product(name, description, price) VALUES ('Suporte de notebook', 'Um suporte de notebook legal', 29.99);
